74071dadeb211b606488f1f0ef1f804a133780cc
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-24 19:33:20 +0300

    Utility methods

---
+++ src/main/java/com/odeyalo/bot/suiri/support/TelegramUtils.java
@@ -0,0 +1,20 @@
+package com.odeyalo.bot.suiri.support;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+public class TelegramUtils {
+
+    public static String getText(Update update) {
+        return update.getMessage().getText();
+
+    }
+
+    public static String getChatId(Update update) {
+        return String.valueOf(update.getMessage().getChatId());
+    }
+
+    public static User getTelegramUser(Update update) {
+        return update.getMessage().getFrom();
+    }
+}
