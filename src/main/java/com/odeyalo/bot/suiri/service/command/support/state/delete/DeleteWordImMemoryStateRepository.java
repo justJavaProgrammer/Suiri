a3017095042cfb629c4893e02ac649ecab45087d
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-24 18:34:13 +0300

    State repo with DeleteWordState

---
+++ src/main/java/com/odeyalo/bot/suiri/service/command/support/state/delete/DeleteWordImMemoryStateRepository.java
@@ -0,0 +1,9 @@
+package com.odeyalo.bot.suiri.service.command.support.state.delete;
+
+import com.odeyalo.bot.suiri.service.command.support.state.ImMemoryStateRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeleteWordImMemoryStateRepository extends ImMemoryStateRepository<DeleteWordState> {
+
+}
