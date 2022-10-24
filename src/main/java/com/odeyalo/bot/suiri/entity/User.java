f4d31e5c04499dbad21e4685baf533441d153997
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-24 18:20:51 +0300

    User entity

--- src/main/java/com/odeyalo/bot/suiri/entity/User.java
+++ src/main/java/com/odeyalo/bot/suiri/entity/User.java
@@ -1,4 +1,28 @@
 package com.odeyalo.bot.suiri.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String telegramId;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Dictionary userDictionary;
 }
