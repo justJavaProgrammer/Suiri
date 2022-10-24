2a0286d6c5225ef4651efa33cd32ec0f0fd26f34
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-24 18:30:01 +0300

    In memory repo to word with AddNewWordState

---
+++ src/main/java/com/odeyalo/bot/suiri/service/command/support/state/InMemoryAddNewWordStateRepository.java
@@ -0,0 +1,8 @@
+package com.odeyalo.bot.suiri.service.command.support.state;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InMemoryAddNewWordStateRepository extends ImMemoryStateRepository<AddNewWordState> implements AddNewWordStateRepository {
+
+}
