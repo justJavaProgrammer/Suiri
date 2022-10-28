d37751a21ba0132768bc9b2b7706e1c1d176b1ef
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-28 22:20:13 +0300

    Fixed typo with NOT NULL property in 'last_name' column

--- src/main/resources/db/migration/V1_1__CREATE_ALL_TABLES.sql
+++ src/main/resources/db/migration/V1_1__CREATE_ALL_TABLES.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users
 (
-    id                 BIGSERIAL PRIMARY KEY,
-    first_name         varchar(255)                       NOT NULL,
-    last_name          varchar(255)                       NOT NULL,
-    telegram_id        varchar(255)                       NOT NULL
+    id          BIGSERIAL PRIMARY KEY,
+    first_name  varchar(255) NOT NULL,
+    last_name   varchar(255),
+    telegram_id varchar(255) NOT NULL
 );
 
 CREATE TABLE dictionary
@@ -15,18 +15,19 @@ CREATE TABLE dictionary
 CREATE TABLE dictionary_item
 (
     id              BIGSERIAL PRIMARY KEY,
-    original_text   varchar(255)                       NOT NULL,
-    picture         varchar(255)                       NOT NULL,
-    translated_text varchar(255)                       NOT NULL,
+    original_text   varchar(255) NOT NULL,
+    picture         varchar(255) NOT NULL,
+    translated_text varchar(255) NOT NULL,
     dictionary_id   BIGINT REFERENCES dictionary (id)
 );
 
 
 CREATE TABLE dictionary_dictionary_item
 (
-    dictionary_id   BIGINT REFERENCES dictionary (id),
-    items_id BIGINT REFERENCES dictionary_item (id)
+    dictionary_id BIGINT REFERENCES dictionary (id),
+    items_id      BIGINT REFERENCES dictionary_item (id)
 
 );
 
-ALTER TABLE users ADD user_dictionary_id BIGINT REFERENCES dictionary (id) NOT NULL;
+ALTER TABLE users
+    ADD user_dictionary_id BIGINT REFERENCES dictionary (id) NOT NULL;
