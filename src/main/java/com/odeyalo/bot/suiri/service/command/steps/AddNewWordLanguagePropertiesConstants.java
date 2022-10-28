0e6f68cfa4bc6fcad48142ea25e6e0498e176f7c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-28 22:17:09 +0300

    Constants that contains property name to access value

--- src/main/java/com/odeyalo/bot/suiri/service/command/steps/AddNewWordLanguagePropertiesConstants.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/steps/AddNewWordLanguagePropertiesConstants.java
@@ -9,5 +9,5 @@ final class AddNewWordLanguagePropertiesConstants {
     public static final String ADD_WORD_ORIGINAL_WORD_STEP_MESSAGE_PROPERTY = "language.word.add.step.original.word";
     public static final String ADD_WORD_TRANSLATED_WORD_STEP_MESSAGE_PROPERTY = "language.word.add.step.translated.word";
     public static final String ADD_WORD_PICTURE_STEP_MESSAGE_PROPERTY = "language.word.add.step.picture";
-    public static final String ADD_WORD_FINISH_STEP_MESSAGE_PROPERTY = "language.word.add.step.picture";
+    public static final String ADD_WORD_FINISH_STEP_MESSAGE_PROPERTY = "language.word.add.step.finish";
 }
---
+++ src/main/java/com/odeyalo/bot/suiri/service/command/steps/settings/SettingsLanguagePropertiesConstants.java
@@ -0,0 +1,9 @@
+package com.odeyalo.bot.suiri.service.command.steps.settings;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SettingsLanguagePropertiesConstants {
+    public static final String DEFAULT_SETTINGS_MESSAGE_PROPERTY = "language.word.settings.default.message";
+}
