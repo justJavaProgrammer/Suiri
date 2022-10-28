52a112324175c27853b6c1fa386ea84fb378734d
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-28 22:17:17 +0300

    Constants that contains property name to access value

---
+++ src/main/java/com/odeyalo/bot/suiri/support/lang/GenericLanguagePropertiesConstants.java
@@ -0,0 +1,11 @@
+package com.odeyalo.bot.suiri.support.lang;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class GenericLanguagePropertiesConstants {
+    public static final String LANGUAGE = "others.words.language";
+    public static final String UKRAINIAN_LANGUAGE = "others.words.language.ukrainian";
+    public static final String ENGLISH_LANGUAGE = "others.words.language.english";
+}
