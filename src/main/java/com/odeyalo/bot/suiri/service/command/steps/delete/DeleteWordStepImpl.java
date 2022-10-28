1ad93ada202fbd8a3889a23634dd7ac61f7935a9
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-28 22:19:41 +0300

    Added multi-language support

--- src/main/java/com/odeyalo/bot/suiri/service/command/steps/delete/DeleteWordStepImpl.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/steps/delete/DeleteWordStepImpl.java
@@ -5,16 +5,18 @@ import com.odeyalo.bot.suiri.entity.Dictionary;
 import com.odeyalo.bot.suiri.entity.DictionaryItem;
 import com.odeyalo.bot.suiri.entity.User;
 import com.odeyalo.bot.suiri.repository.DictionaryItemRepository;
-import com.odeyalo.bot.suiri.repository.DictionaryRepository;
 import com.odeyalo.bot.suiri.repository.UserRepository;
 import com.odeyalo.bot.suiri.service.command.support.state.delete.DeleteWordState;
 import com.odeyalo.bot.suiri.support.TelegramUtils;
+import com.odeyalo.bot.suiri.support.lang.ResponseMessageResolverDecorator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static com.odeyalo.bot.suiri.service.command.steps.delete.DeleteWordLanguagePropertiesConstants.DELETE_WORD_PROPERTY;
 
 /**
  * Delete the word from user's dictionary
@@ -23,11 +25,13 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class DeleteWordStepImpl implements DeleteWordStep {
     private final DictionaryItemRepository dictionaryItemRepository;
     private final UserRepository userRepository;
+    private final ResponseMessageResolverDecorator responseMessageResolverDecorator;
 
     @Autowired
-    public DeleteWordStepImpl(DictionaryItemRepository dictionaryItemRepository, UserRepository userRepository) {
+    public DeleteWordStepImpl(DictionaryItemRepository dictionaryItemRepository, UserRepository userRepository, ResponseMessageResolverDecorator responseMessageResolverDecorator) {
         this.dictionaryItemRepository = dictionaryItemRepository;
         this.userRepository = userRepository;
+        this.responseMessageResolverDecorator = responseMessageResolverDecorator;
     }
 
     @Override
@@ -37,9 +41,9 @@ public class DeleteWordStepImpl implements DeleteWordStep {
         String word = TelegramUtils.getText(update);
 
         Dictionary userDictionary = deleteWordAndGetDictionary(userId, word);
-
+        String responseMessage = this.responseMessageResolverDecorator.getResponseMessage(update, DELETE_WORD_PROPERTY);
         this.dictionaryItemRepository.deleteDictionaryItemByDictionaryAndOriginalText(userDictionary, word);
-        return new SendMessage(TelegramUtils.getChatId(update), String.format("The word: %s was successful deleted!", word));
+        return new SendMessage(TelegramUtils.getChatId(update), responseMessage);
     }
 
     @Override
