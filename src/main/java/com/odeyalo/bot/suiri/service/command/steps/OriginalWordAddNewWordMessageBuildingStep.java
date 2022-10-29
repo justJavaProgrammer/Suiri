796a61674a51ca2a98fa79bcf26530a2e6694bd3
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-29 22:30:50 +0300

    Added suggested words for next state

--- src/main/java/com/odeyalo/bot/suiri/service/command/steps/OriginalWordAddNewWordMessageBuildingStep.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/steps/OriginalWordAddNewWordMessageBuildingStep.java
@@ -3,6 +3,7 @@ package com.odeyalo.bot.suiri.service.command.steps;
 import com.odeyalo.bot.suiri.domain.AddNewWordMessage;
 import com.odeyalo.bot.suiri.service.command.support.state.AddNewWordState;
 import com.odeyalo.bot.suiri.service.command.support.state.AddNewWordStateRepository;
+import com.odeyalo.bot.suiri.service.command.support.translate.TranslatedWordSuggester;
 import com.odeyalo.bot.suiri.support.TelegramUtils;
 import com.odeyalo.bot.suiri.support.lang.ResponseMessageResolverDecorator;
 import org.slf4j.Logger;
@@ -12,16 +13,24 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 
 import static com.odeyalo.bot.suiri.service.command.steps.AddNewWordLanguagePropertiesConstants.ADD_WORD_ORIGINAL_WORD_STEP_MESSAGE_PROPERTY;
 
 @Component
 public class OriginalWordAddNewWordMessageBuildingStep extends AbstractAddNewWordMessageBuildingStep {
     private final Logger logger = LoggerFactory.getLogger(OriginalWordAddNewWordMessageBuildingStep.class);
+    private final TranslatedWordSuggester translatedWordSuggester;
 
     @Autowired
-    public OriginalWordAddNewWordMessageBuildingStep(AddNewWordStateRepository stateRepository, ResponseMessageResolverDecorator responseMessageResolverDecorator) {
+    public OriginalWordAddNewWordMessageBuildingStep(AddNewWordStateRepository stateRepository, ResponseMessageResolverDecorator responseMessageResolverDecorator, TranslatedWordSuggester translatedWordSuggester) {
         super(stateRepository, responseMessageResolverDecorator);
+        this.translatedWordSuggester = translatedWordSuggester;
     }
 
     @Override
@@ -36,11 +45,27 @@ public class OriginalWordAddNewWordMessageBuildingStep extends AbstractAddNewWor
         this.logger.info("Received original word: {}", text);
         message.setOriginalWord(text);
         String responseMessage = this.responseMessageResolverDecorator.getResponseMessage(update, ADD_WORD_ORIGINAL_WORD_STEP_MESSAGE_PROPERTY);
-        return new SendMessage(chatId, responseMessage);
+        SendMessage sendMessage = new SendMessage(chatId, responseMessage);
+        ReplyKeyboardMarkup keyboard = getTranslatedWordSuggestionKeyboard(text, responseMessageResolverDecorator.getUserLanguage(update));
+        sendMessage.setReplyMarkup(keyboard);
+        return sendMessage;
     }
 
     @Override
     public AddNewWordState getState() {
         return AddNewWordState.ORIGINAL_WORD;
     }
+
+
+    private ReplyKeyboardMarkup getTranslatedWordSuggestionKeyboard(String originalWord, String userLanguage){
+        Set<String> words = this.translatedWordSuggester.suggestWords(originalWord, userLanguage);
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setOneTimeKeyboard(true);
+        keyboardMarkup.setResizeKeyboard(true);
+        KeyboardRow buttons = new KeyboardRow();
+        buttons.addAll(new ArrayList<>(words));
+        keyboardMarkup.setKeyboard(Collections.singletonList(buttons));
+        return keyboardMarkup;
+    }
+
 }
