c1ae16bb4ae224fd4ebec5a721206c8a31087102
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-02 13:39:34 +0200

    Added findAllByUserSettingsEnableNotification method

--- src/main/java/com/odeyalo/bot/suiri/repository/UserRepository.java
+++ src/main/java/com/odeyalo/bot/suiri/repository/UserRepository.java
@@ -4,8 +4,22 @@ import com.odeyalo.bot.suiri.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    /**
+     * Find user by telegram id
+     * @param telegramId - user's telegram id
+     * @return - user by telegram id
+     */
     User findUserByTelegramId(String telegramId);
+
+    /**
+     * Search users by enableNotification param
+     * @param enableNotification - user notification's settings
+     * @return - list of users searched by this param
+     */
+    List<User> findAllByUserSettingsEnableNotification(boolean enableNotification);
 }
