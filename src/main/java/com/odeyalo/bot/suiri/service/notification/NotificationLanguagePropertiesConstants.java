c800f69ae92c1422f7641ab715d71a93442c55c3
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-02 13:34:31 +0200

    Simple support class that holds constant for bot dictionary with notification properties

---
+++ src/main/java/com/odeyalo/bot/suiri/service/notification/NotificationLanguagePropertiesConstants.java
@@ -0,0 +1,8 @@
+package com.odeyalo.bot.suiri.service.notification;
+
+/**
+ * Simple support class that holds constant for bot dictionary with notification properties
+ */
+public final class NotificationLanguagePropertiesConstants {
+    public static final String REMAINDER_NOTIFICATION_MESSAGE_PROPERTY_KEY = "others.words.notification.reminder";
+}
