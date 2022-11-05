867228dcf11b99d314391042a52ea3a275cb425f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-05 16:11:44 +0200

    Interface that create test to test user knowledge

---
+++ src/main/java/com/odeyalo/bot/suiri/service/command/support/test/UserWordKnowledgeTestingManager.java
@@ -0,0 +1,22 @@
+package com.odeyalo.bot.suiri.service.command.support.test;
+
+import com.odeyalo.bot.suiri.entity.User;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Interface that test user knowledge
+ */
+public interface UserWordKnowledgeTestingManager {
+
+    /**
+     * Method to create and return the test for user.
+     * If current manager does not support BotApiMethod return type will be returned null
+     * @param update - incoming message update
+     * @param user - user to create test
+     * @return - user words knowledge test that contains in user dictionary
+     */
+    BotApiMethod<?> getUserKnowledgeTest(Update update, User user);
+
+
+}
