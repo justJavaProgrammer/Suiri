8c5a2f662d16aa7aa2c4abb43a8290556a819e0b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-05 16:12:50 +0200

    Data class that contains properties for test

---
+++ src/main/java/com/odeyalo/bot/suiri/service/command/support/test/UserDictionaryKnowledgeTest.java
@@ -0,0 +1,23 @@
+package com.odeyalo.bot.suiri.service.command.support.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.SortedSet;
+
+/**
+ *  Data class that contains options, correct answer, picture, original text and explanation to test
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDictionaryKnowledgeTest {
+    private String picture;
+    private String originalText;
+    private String correctAnswer;
+    private SortedSet<String> options;
+    private String explanation;
+}
