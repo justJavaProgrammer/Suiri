f5805a32937ba2cfddfa29c645b9b41fc3c8358b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-05 16:10:55 +0200

    Container replaced with ResponseMessageResolverHelper

--- src/main/java/com/odeyalo/bot/suiri/support/lang/ResponseMessageResolverDecoratorImpl.java
+++ src/main/java/com/odeyalo/bot/suiri/support/lang/ResponseMessageResolverDecoratorImpl.java
@@ -4,23 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-
+/**
+ * Decorator that resolve user's language and return message
+ * @see com.odeyalo.bot.suiri.support.lang.ResponseMessageResolverDecorator
+ */
 @Service
 public class ResponseMessageResolverDecoratorImpl implements ResponseMessageResolverDecorator {
-    private final LanguageResponseMessageResolverRegistry container;
+    private final ResponseMessageResolverHelper responseMessageResolverHelper;
     private final UserLocaleLanguageResolver languageResolver;
 
     @Autowired
-    public ResponseMessageResolverDecoratorImpl(LanguageResponseMessageResolverRegistry container, UserLocaleLanguageResolver languageResolver) {
-        this.container = container;
+    public ResponseMessageResolverDecoratorImpl(ResponseMessageResolverHelper responseMessageResolverHelper, UserLocaleLanguageResolver languageResolver) {
+        this.responseMessageResolverHelper = responseMessageResolverHelper;
         this.languageResolver = languageResolver;
     }
 
     @Override
     public String getResponseMessage(Update update, String property) {
         String languageCode = languageResolver.resolveLang(update);
-        LanguageResponseMessageResolverStrategy resolver = container.getOrDefault(languageCode, Languages.ENGLISH);
-        return resolver.getMessage(property);
+        return responseMessageResolverHelper.getMessageByLanguageCode(languageCode, property);
     }
 
     @Override
