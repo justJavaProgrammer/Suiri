221477b56b08f3b72b4d93eb64698ef72554e56b
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-08 15:43:05 +0200

    Added props

--- src/main/java/com/odeyalo/bot/suiri/service/command/steps/settings/SettingsLanguagePropertiesConstants.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/steps/settings/SettingsLanguagePropertiesConstants.java
@@ -9,4 +9,10 @@ public final class SettingsLanguagePropertiesConstants {
     public static final String NOTIFICATION_SETTINGS_PROPERTY = "language.settings.notification";
     public static final String ON_NOTIFICATION_DISABLED_PROPERTY = "language.settings.notification.message.on.disable";
     public static final String ON_NOTIFICATION_ENABLED_PROPERTY = "language.settings.notification.message.on.enable";
+    public static final String PREFERRED_TEST_TYPE_VALUE = "language.settings.preferred.test.type.value";
+    public static final String PREFERRED_TEST_TYPE_ON_START_MESSAGE = "language.settings.preferred.test.type.on.start";
+    public static final String PREFERRED_TEST_TYPE_QUIZ_POLL = "language.settings.preferred.test.type.quiz.poll";
+    public static final String PREFERRED_TEST_TYPE_UPDATABLE_MESSAGE = "language.settings.preferred.test.type.updatable.message";
+    public static final String PREFERRED_TEST_TYPE_ON_CHANGED = "language.settings.preferred.test.type.on.changed.message";
+    public static final String PREFERRED_TEST_TYPE_ON_FINISH = "language.settings.preferred.test.type.on.finish";
 }
