5c18aa767fac64a769afb2c7592021e5288c35bb
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-08 15:40:44 +0200

    Replaced with strategy

--- src/main/java/com/odeyalo/bot/suiri/service/command/TestCommandExecutor.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/TestCommandExecutor.java
@@ -2,12 +2,11 @@ package com.odeyalo.bot.suiri.service.command;
 
 import com.odeyalo.bot.suiri.entity.User;
 import com.odeyalo.bot.suiri.repository.UserRepository;
-import com.odeyalo.bot.suiri.service.command.support.test.UserWordKnowledgeTestingManager;
+import com.odeyalo.bot.suiri.service.command.support.test.UserWordKnowledgeTestingManagerStrategy;
 import com.odeyalo.bot.suiri.support.TelegramUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -22,22 +21,23 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class TestCommandExecutor implements CommandExecutor {
     private static final String COMMAND_NAME = "/test";
     private final UserRepository userRepository;
-    private final UserWordKnowledgeTestingManager userWordKnowledgeTestingManager;
+    private final UserWordKnowledgeTestingManagerStrategy userWordKnowledgeTestingManagerStrategy;
 
     private final Logger logger = LoggerFactory.getLogger(TestCommandExecutor.class);
 
     @Autowired
-    public TestCommandExecutor(UserRepository userRepository, @Qualifier("updatableMessageUserWordKnowledgeTestingManager") UserWordKnowledgeTestingManager userWordKnowledgeTestingManager) {
+    public TestCommandExecutor(UserRepository userRepository, UserWordKnowledgeTestingManagerStrategy userWordKnowledgeTestingManagerStrategy) {
         this.userRepository = userRepository;
-        this.userWordKnowledgeTestingManager = userWordKnowledgeTestingManager;
+        this.userWordKnowledgeTestingManagerStrategy = userWordKnowledgeTestingManagerStrategy;
     }
 
     @Override
     public BotApiMethod<?> execute(Update update) {
         Long telegramId = TelegramUtils.getTelegramUser(update).getId();
         User user = this.userRepository.findUserByTelegramId(String.valueOf(telegramId));
+        this.logger.info("Execute /test command for user: {} ", user);
 
-        return (BotApiMethod<?>) userWordKnowledgeTestingManager.getUserKnowledgeTest(update, user);
+        return (BotApiMethod<?>) userWordKnowledgeTestingManagerStrategy.getUserKnowledgeTest(update, user);
     }
 
     @Override
