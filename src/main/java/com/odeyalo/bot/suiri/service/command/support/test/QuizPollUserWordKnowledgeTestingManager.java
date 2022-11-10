abe0f921d6b30a2a592f5a8084e5b116aa5a403c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-11-10 10:47:16 +0200

    Added InputMediaSenderDelegate

--- src/main/java/com/odeyalo/bot/suiri/service/command/support/test/QuizPollUserWordKnowledgeTestingManager.java
+++ src/main/java/com/odeyalo/bot/suiri/service/command/support/test/QuizPollUserWordKnowledgeTestingManager.java
@@ -1,18 +1,18 @@
 package com.odeyalo.bot.suiri.service.command.support.test;
 
+import com.odeyalo.bot.suiri.entity.DictionaryItem;
 import com.odeyalo.bot.suiri.entity.User;
 import com.odeyalo.bot.suiri.entity.UserSettings;
 import com.odeyalo.bot.suiri.service.command.support.QuizPollBuilderHelper;
+import com.odeyalo.bot.suiri.service.command.support.media.InputMediaBuilderHelper;
+import com.odeyalo.bot.suiri.service.command.support.media.sender.InputMediaSenderDelegate;
 import com.odeyalo.bot.suiri.support.TelegramUtils;
 import com.odeyalo.bot.suiri.support.lang.ResponseMessageResolverDecorator;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
@@ -27,14 +27,16 @@ import static com.odeyalo.bot.suiri.service.command.support.TestUserKnowledgeLan
 @Service
 public class QuizPollUserWordKnowledgeTestingManager extends AbstractUserWordKnowledgeTestingManager {
     private final QuizPollBuilderHelper quizPollBuilderHelper;
+    private final InputMediaSenderDelegate senderDelegate;
 
     @Autowired
     public QuizPollUserWordKnowledgeTestingManager(AbsSender absSender,
                                                    QuizPollBuilderHelper quizPollBuilderHelper,
                                                    ResponseMessageResolverDecorator responseMessageResolverDecorator,
-                                                   UserDictionaryKnowledgeTestGenerator userDictionaryKnowledgeTestGenerator) {
-        super(userDictionaryKnowledgeTestGenerator, responseMessageResolverDecorator, absSender);
+                                                   UserDictionaryKnowledgeTestGenerator userDictionaryKnowledgeTestGenerator, InputMediaBuilderHelper builderHelper, InputMediaSenderDelegate senderDelegate) {
+        super(userDictionaryKnowledgeTestGenerator, responseMessageResolverDecorator, absSender, builderHelper);
         this.quizPollBuilderHelper = quizPollBuilderHelper;
+        this.senderDelegate = senderDelegate;
     }
 
     @Override
@@ -48,23 +50,18 @@ public class QuizPollUserWordKnowledgeTestingManager extends AbstractUserWordKno
 
         UserDictionaryKnowledgeTest test = userDictionaryKnowledgeTestGenerator.generateTest(user);
 
-        sendWordPhoto(chatId, test.getPicture());
+        sendByPictureType(chatId, test.getPictureType(), test.getPicture());
 
         return getPoll(chatId, test.getOriginalText(), test.getExplanation(), test.getCorrectAnswer(), update, test.getOptions());
+    }
+
+    private void sendByPictureType(String chatId, DictionaryItem.PictureType pictureType, String picture) {
+        this.senderDelegate.sendMedia(chatId, picture, pictureType.name());
     }
 
     @Override
     public UserSettings.PreferredKnowledgeTestType getType() {
         return UserSettings.PreferredKnowledgeTestType.QUIZ_POLL;
-    }
-
-    @SneakyThrows
-    private void sendWordPhoto(String chatId, String picture) {
-        SendPhoto sendPhoto = SendPhoto.builder()
-                .photo(new InputFile(picture))
-                .chatId(chatId)
-                .build();
-        absSender.execute(sendPhoto);
     }
 
     private SendPoll getPoll(String chatId, String originalText, String explanation, String correctAnswer, Update update, SortedSet<String> options) {
